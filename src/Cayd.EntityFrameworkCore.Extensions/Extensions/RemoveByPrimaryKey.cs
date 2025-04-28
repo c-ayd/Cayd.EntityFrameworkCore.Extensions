31756dc555b5ce839bf162f06efbe05be4992e85
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-28 20:15:13 +0200

    add remove by primary key

---
+++ src/Cayd.EntityFrameworkCore.Extensions/Extensions/RemoveByPrimaryKey.cs
@@ -0,0 +1,21 @@
+﻿using Cayd.EntityFrameworkCore.Extensions.Utility;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cayd.EntityFrameworkCore.Extensions
+{
+    public static partial class EFCoreExtensions
+    {
+        public static void RemoveByPrimaryKey<TEntity>(this DbSet<TEntity> dbSet, object primaryKey)
+            where TEntity : class
+        {
+            dbSet.GetDbContext().RemoveByPrimaryKey<TEntity>(primaryKey);
+        }
+
+        public static void RemoveByPrimaryKey<TEntity>(this DbContext dbContext, object primaryKey)
+            where TEntity : class
+        {
+            var entity = dbContext.GetEntityByPrimaryKey<TEntity>(primaryKey);
+            dbContext.Remove(entity);
+        }
+    }
+}
---
+++ test/IntegrationTests/Cayd.EntityFrameworkCore.Extensions.Test.Integration/Extensions/RemoveByPrimaryKeyTest.cs
@@ -0,0 +1,178 @@
+﻿using Cayd.EntityFrameworkCore.Extensions.Test.Api.DbContexts;
+using Cayd.EntityFrameworkCore.Extensions.Test.Api.Entities;
+using Cayd.EntityFrameworkCore.Extensions.Test.Api.Entities.ValueObjects;
+using Cayd.EntityFrameworkCore.Extensions.Test.Integration.Collections.DbContexts;
+using Cayd.EntityFrameworkCore.Extensions.Test.Utility.Fixtures;
+using Cayd.EntityFrameworkCore.Extensions.Test.Utility.Helpers;
+using Cayd.Test.Generators;
+using System.Linq.Expressions;
+
+namespace Cayd.EntityFrameworkCore.Extensions.Test.Integration.Extensions
+{
+    [Collection(nameof(TestDbContextCollection))]
+    public class RemoveByPrimaryKeyTest
+    {
+        private readonly TestDbContext _dbContext;
+
+        public RemoveByPrimaryKeyTest(TestDbContextFixture dbContextFixture)
+        {
+            _dbContext = dbContextFixture.TestDbContext;
+        }
+
+        [Fact]
+        public async void RemoveByPrimaryKey_WhenKeyIsSimpleAndEntityIsInChangeTrackerAndEntityIsRemovedViaDbContext_ShouldRemoveEntity()
+        {
+            // Arrange
+            var entity = ClassGenerator.Generate<TestParentEntity>();
+
+            _dbContext.TestParents.Add(entity);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            _dbContext.RemoveByPrimaryKey<TestParentEntity>(entity.Id);
+
+            await _dbContext.SaveChangesAsync();
+            var result = await _dbContext.TestParents.FindAsync(entity.Id);
+
+            // Arrange
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void RemoveByPrimaryKey_WhenKeyIsSimpleAndEntityIsInChangeTrackerAndEntityIsRemovedViaDbSet_ShouldRemoveEntity()
+        {
+            // Arrange
+            var entity = ClassGenerator.Generate<TestParentEntity>();
+
+            _dbContext.TestParents.Add(entity);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            _dbContext.TestParents.RemoveByPrimaryKey(entity.Id);
+
+            await _dbContext.SaveChangesAsync();
+            var result = await _dbContext.TestParents.FindAsync(entity.Id);
+
+            // Arrange
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void RemoveByPrimaryKey_WhenKeyIsSimpleAndEntityIsNotInChangeTrackerAndEntityIsRemovedViaDbContext_ShouldRemoveEntity()
+        {
+            // Arrange
+            var entity = ClassGenerator.Generate<TestParentEntity>();
+
+            _dbContext.TestParents.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            _dbContext.UntrackEntity(entity);
+
+            // Act
+            _dbContext.RemoveByPrimaryKey<TestParentEntity>(entity.Id);
+
+            await _dbContext.SaveChangesAsync();
+            var result = await _dbContext.TestParents.FindAsync(entity.Id);
+
+            // Arrange
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void RemoveByPrimaryKey_WhenKeyIsSimpleAndEntityIsNotInChangeTrackerAndEntityIsRemovedViaDbSet_ShouldRemoveEntity()
+        {
+            // Arrange
+            var entity = ClassGenerator.Generate<TestParentEntity>();
+
+            _dbContext.TestParents.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            _dbContext.UntrackEntity(entity);
+
+            // Act
+            _dbContext.TestParents.RemoveByPrimaryKey(entity.Id);
+
+            await _dbContext.SaveChangesAsync();
+            var result = await _dbContext.TestParents.FindAsync(entity.Id);
+
+            // Arrange
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void RemoveByPrimaryKey_WhenKeyIsCompositeAndEntityIsInChangeTrackerAndEntityIsRemovedViaDbContext_ShouldRemoveEntity()
+        {
+            // Arrange
+            var entity = ClassGenerator.Generate<TestCompositeEntity>();
+
+            _dbContext.TestComposites.Add(entity);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            _dbContext.RemoveByPrimaryKey<TestCompositeEntity>(new { entity.Id1, entity.Id2 });
+
+            await _dbContext.SaveChangesAsync();
+            var result = await _dbContext.TestComposites.FindAsync(entity.Id1, entity.Id2);
+
+            // Arrange
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void RemoveByPrimaryKey_WhenKeyIsCompositeAndEntityIsInChangeTrackerAndEntityIsRemovedViaDbSet_ShouldRemoveEntity()
+        {
+            // Arrange
+            var entity = ClassGenerator.Generate<TestCompositeEntity>();
+
+            _dbContext.TestComposites.Add(entity);
+            await _dbContext.SaveChangesAsync();
+
+            // Act
+            _dbContext.TestComposites.RemoveByPrimaryKey(new { entity.Id1, entity.Id2 });
+
+            await _dbContext.SaveChangesAsync();
+            var result = await _dbContext.TestComposites.FindAsync(entity.Id1, entity.Id2);
+
+            // Arrange
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void RemoveByPrimaryKey_WhenKeyIsCompositeAndEntityIsNotInChangeTrackerAndEntityIsRemovedViaDbContext_ShouldRemoveEntity()
+        {
+            // Arrange
+            var entity = ClassGenerator.Generate<TestCompositeEntity>();
+
+            _dbContext.TestComposites.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            _dbContext.UntrackEntity(entity);
+
+            // Act
+            _dbContext.RemoveByPrimaryKey<TestCompositeEntity>(new { entity.Id1, entity.Id2 });
+
+            await _dbContext.SaveChangesAsync();
+            var result = await _dbContext.TestComposites.FindAsync(entity.Id1, entity.Id2);
+
+            // Arrange
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void RemoveByPrimaryKey_WhenKeyIsCompositeAndEntityIsNotInChangeTrackerAndEntityIsRemovedViaDbSet_ShouldRemoveEntity()
+        {
+            // Arrange
+            var entity = ClassGenerator.Generate<TestCompositeEntity>();
+
+            _dbContext.TestComposites.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            _dbContext.UntrackEntity(entity);
+
+            // Act
+            _dbContext.TestComposites.RemoveByPrimaryKey(new { entity.Id1, entity.Id2 });
+
+            await _dbContext.SaveChangesAsync();
+            var result = await _dbContext.TestComposites.FindAsync(entity.Id1, entity.Id2);
+
+            // Arrange
+            Assert.Null(result);
+        }
+    }
+}
